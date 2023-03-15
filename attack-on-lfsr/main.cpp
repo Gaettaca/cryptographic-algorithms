4d2a034e02e0c45078bdaae1ab00786e03ac3a47
Daniel <theaquilarift@gmail.com>
2023-03-16 00:14:54 +0300

    random generators

--- attack-on-lfsr/main.cpp
+++ attack-on-lfsr/main.cpp
@@ -4,71 +4,6 @@
 #include <vector>
 #include "LFSR.h"
 
-void lfsr() {
-    uint16_t S = 17590;
-    for (size_t i = 1; i < 65; ++i) {
-        std::cout << int(S & 1);
-        S = (((S >> 15) ^ (S >> 12) ^ (S >> 10) ^ S) << 15) | (S >> 1);
-        if (i % 8 == 0) { std::cout << "\n"; }
-    }
-}
-
-void OpenTestFile() {
-//    std::ofstream fout("out-test.txt");
-
-    for (size_t i = 17584; i < 17600; ++i) {
-        std::ifstream fin("out-test.txt", std::ios::binary);
-        LFSR gen(i, {15, 12, 10});
-        char c;
-        fin >> std::noskipws;
-        std::cout << ">> " << i << " ";
-        while (fin >> c) {
-            std::cout << char(c ^ (char) gen.GenerateWord().to_ulong());
-        }
-        std::cout << "\n";
-    }
-}
-
-void RussianTextOpenFile() {
-    std::ifstream fin("rus.txt", std::ios::binary);
-    std::ofstream fout("out-rus.txt");
-    uint8_t c;
-    while (fin >> c) {
-        fout << c << "";
-    }
-}
-
-void LuEncryption() {
-    std::ifstream fin("Lu.fb2", std::ios::binary);
-    std::ofstream fout("Lu-out.fb2");
-
-    char c;
-    fin >> std::noskipws;
-    LFSR gen(21089, {15, 12, 10});
-    for (size_t i = 0; i < 100; ++i) {
-        fin >> c;
-        fout << char (c ^ (char) gen.GenerateWord().to_ulong());
-    }
-    fin.close();
-    fout.close();
-}
-
-void LuDecryption() {
-    std::ofstream fout("Lu-out-dec.fb2");
-
-
-    for (size_t k = 21089; k < 21090; ++k) {
-        std::ifstream fin("Lu-out.fb2", std::ios::binary);
-        fin >> std::noskipws;
-        LFSR gen(k, {15, 12, 10});
-        char c;
-        for (size_t i = 0; i < 100; ++i) {
-            fin >> c;
-            fout << char(c ^ (char) gen.GenerateWord().to_ulong());
-        }
-    }
-    fout.close();
-}
 
 void FindInitial() {
     std::ofstream fout("ct3-init-out.fb2");
@@ -106,16 +41,5 @@ void Decode() {
 int main() {
     FindInitial();
 //    Decode();
-
-//    LuEncryption();
-//    LuDecryption();
-
-//    OpenTestFile();
-//    RussianTextOpenFile();
-
-//    LFSR gen(17590, {15, 12, 10});
-//    for (size_t i = 0; i < 13; ++i) {
-//        std::cout << gen.GenerateWord() << "\n";
-//    }
     return 0;
 }
